25f32bb96c6ab274132b53cd166b0c5c089a6acb
misza314 <michalzejmo@gmail.com>
2016-05-18 22:10:05 +0200

    histo+multiview

--- main.py
+++ main.py
@@ -20,14 +20,16 @@ def loadData(fName):
     return fData
 
 
-def addHist(img):
+def createHist(image):
     hist = pg.HistogramLUTItem()
-    hist.setImageItem(img)
-    win.addItem(hist)
+    hist.setImageItem(image)
+    # hist.setLevels(fData.min(), fData.max())
+    return hist
 
 
 def update(roi):
-    img1b.setImage(roi.getArrayRegion(fData, img1a), levels=(0, fData.max()))
+    img1b.setImage(roi.getArrayRegion(fData, img))
+    hist.setImageItem(img1b)
     v1b.autoRange()
 
 
@@ -37,22 +39,23 @@ if __name__ == '__main__':
     img = pg.ImageItem()
     img.setImage(fData)
     # p1.addItem(img)
-    win.resize(fData.shape[0], fData.shape[1])
+    # win.resize(fData.shape[0], fData.shape[1])
     # addHist(img)
     # win.nextRow()
-    w1 = win.addLayout(row=0, col=0)
-    label1 = w1.addLabel('ss', row=0, col=0)
-    v1a = w1.addViewBox(row=1, col=0, lockAspect=True)
-    v1b = w1.addViewBox(row=2, col=0, lockAspect=True)
-    img1a = pg.ImageItem(fData)
-    v1a.addItem(img1a)
+    w1 = win.addLayout(row=2, col=2)
+    v1a = w1.addViewBox(row=1, col=1, rowspan=1, colspan=2, lockAspect=True)
+    hist = pg.HistogramLUTItem()
+    v2a = w1.addItem(hist, row=2, col=2)
+    v1b = w1.addViewBox(row=2, col=1, lockAspect=True)
+    v1a.addItem(img)
+    # v2a.addItem(createHist())
     img1b = pg.ImageItem()
     v1b.addItem(img1b)
     v1a.disableAutoRange('xy')
     v1b.disableAutoRange('xy')
     v1a.autoRange()
     v1b.autoRange()
-    roi = pg.RectROI([20, 20], [20, 20], pen=(0, 9))
+    roi = pg.RectROI([200, 200], [200, 200], pen=(0, 9))
     roi.sigRegionChanged.connect(update)
     v1a.addItem(roi)
     update(roi)
