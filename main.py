6b1c48f08eb113d0984305a457ebc08d3a0b726f
misza314 <michalzejmo@gmail.com>
2016-05-23 15:01:22 +0200

    button, qtmode

--- main.py
+++ main.py
@@ -18,9 +18,10 @@ script_path = os.path.dirname(os.path.realpath(__file__))
 work_dir = os.path.curdir
 
 
-pg.mkQApp()
-win = pg.GraphicsLayoutWidget()
-
+# pg.mkQApp()
+# win = pg.GraphicsWindow()
+app = QtGui.QApplication([])
+win = QtGui.QWidget()
 
 def loadData(fName):
     fData = io.imread(fName)
@@ -36,12 +37,25 @@ def createHist(image):
     return hist
 
 
-def update(roi):
+def update():
     roi_arr = roi.getArrayRegion(fData, img)
-    img1b.setImage(roi_arr)
-    hist.setImageItem(img1b)
-    v1b.autoRange()
+    roiImage.setImage(roi_arr)
+    histogram.setImageItem(roiImage)
+    roiPlot.autoRange()
     otsu(roi_arr)
+
+
+def updateContours():
+    if rcheck.isChecked():
+        roi_arr = roi.getArrayRegion(fData, img)
+        contours = otsu(roi_arr)
+        for c in contours:
+            c = c.astype(int)
+            roi_arr[c[:, 0], c[:, 1]] = 1
+        roiImage.setImage(roi_arr)
+    else:
+        roi_arr = roi.getArrayRegion(fData, img)
+        roiImage.setImage(roi_arr)
 
 
 def otsu(roi_arr):
@@ -52,12 +66,15 @@ def otsu(roi_arr):
     for c in contours:
         c = c.astype(int)
         otsu_arr[c[:, 0], c[:, 1]] = 3
-    img3b.setImage(otsu_arr)
-    v3b.autoRange()
+    contourImage.setImage(otsu_arr)
+    contourPlot.autoRange()
+
+    return contours
 
 
 def makeContour(otsu_arr):
     contours = measure.find_contours(otsu_arr, 0.99)
+
     return contours
 
 
@@ -65,33 +82,49 @@ if __name__ == '__main__':
     fData = loadData("1.tif")
     img = pg.ImageItem()
     img.setImage(fData)
-    win.resize(800, 1200)
+    win.resize(1000, 1200)
 
-    w1 = win.addLayout(row=2, col=3)
-    v1a = w1.addViewBox(row=1, col=1, rowspan=1, colspan=3,
-                        lockAspect=True, enableMouse=False)
-    hist = pg.HistogramLUTItem()
-    v2a = w1.addItem(hist, row=2, col=1)
-    v3b = w1.addViewBox(row=2, col=3, lockAspect=True, enableMouse=False)
-    v1b = w1.addViewBox(row=2, col=2, lockAspect=True, enableMouse=False)
-    v1a.addItem(img)
-    img3b = pg.ImageItem()
-    img1b = pg.ImageItem()
-    v1b.addItem(img1b)
-    v3b.addItem(img3b)
-    v1a.disableAutoRange('xy')
-    v1b.disableAutoRange('xy')
-    v3b.disableAutoRange('xy')
-    v1a.autoRange()
-    v1b.autoRange()
-    v3b.autoRange()
+    # w1 = win.addLayout(row=3, col=3)
+    w1 = QtGui.QGridLayout()
+    win.setLayout(w1)
+
+    mainPlot = pg.PlotWidget()
+    mainPlot.setAspectLocked()
+    mainPlot.setMouseEnabled(x=False, y=False)
+
+    histogram = pg.HistogramLUTWidget()
+
+    roiPlot = pg.PlotWidget()
+    roiPlot.setAspectLocked()
+    roiPlot.setMouseEnabled(x=False, y=False)
+
+    contourPlot = pg.PlotWidget()
+    contourPlot.setAspectLocked()
+    contourPlot.setMouseEnabled(x=False, y=False)
+
+    rcheck = QtGui.QCheckBox('Plot contour')
+
+    w1.addWidget(mainPlot, 0, 0, 1, 3)
+    w1.addWidget(histogram, 1, 0)
+    w1.addWidget(roiPlot, 1, 1)
+    w1.addWidget(contourPlot, 1, 2)
+    w1.addWidget(rcheck, 2, 1)
+    # win.addLayout(rcheck)
+
+    mainPlot.addItem(img)
+    roiImage = pg.ImageItem()
+    roiPlot.addItem(roiImage)
+    contourImage = pg.ImageItem()
+    contourPlot.addItem(contourImage)
+
     roi = pg.RectROI([100, 100], [100, 100], pen=(0, 9))
-    # roi = pg.EllipseROI([200, 200], [120, 120])
     roi.sigRegionChanged.connect(update)
-    v1a.addItem(roi)
-    update(roi)
+
+    mainPlot.addItem(roi)
+
+    timer = QtCore.QTimer()
+    timer.timeout.connect(updateContours)
+    timer.start(0)
 
     win.show()
-    # start loop
-    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
-        QtGui.QApplication.instance().exec_()
+    app.exec_()
